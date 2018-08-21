546a7d159c299d9f8d9fcb85fef50e4d740f2134
swisshill60 <swisshill60@gmail.com>
2018-08-21 03:00:16 +0100

    modification

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,11 @@ var db = require('monk')('localhost/nodeblog');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var db = req.db;
+    var posts = db.get('posts');
+    posts.find({}, {}, function (err, posts) {
+        res.render('index', { posts: posts });
+    });
 });
 
 module.exports = router;
